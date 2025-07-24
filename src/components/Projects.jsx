dfad007c5799b4ee36ff9df63eabb745c87e74a1
omkothari <omkothari2004@gmail.com>
2025-07-25 01:35:40 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,16 +26,16 @@ const Projects = () => {
 
 
   return (
-    <div id="projects" className='px-8'>
+    <div id="projects" className='px-8 min-h-[47vh]'>
         <h1 className="text-2xl font-bold mt-30 mb-10">
         Projects
         </h1>
 
-        <div className='box min-h-[40vh] w-full flex shrink-0 gap-10 overflow-x-auto'>
+        <div className='box min-h-[45vh] max-h-fit w-full flex shrink-0 gap-10 overflow-x-auto'>
 
             {projects.map((project, index) => (
-                 <div className=" min-w-[60vh] w-[40vw] bg-black rounded-md gap-4 shrink-0" key={index}>
-                <div className="img bg-white w-full h-full rounded-lg overflow-hidden hover:scale-105 hover:mb-2 transition-all duration-300">
+                 <div className="w-fit rounded-md gap-4 shrink-0" key={index}>
+                <div className="img bg-white min-w-[60vh] w-[40vw] rounded-lg overflow-hidden hover:scale-105 hover:mb-2 transition-all duration-300">
                     <img src={project.image} className={`object-contain w-full h-full`} alt="" />
                 </div>
                 <h1 className="text-lg font-bold text-[#222222]">{project.heading}</h1>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
-  plugins: [react(),
-    tailwindcss(),
-  ],
-})
+  base: "/portfolio/", // Required for GitHub Pages
+  plugins: [react()],
+});
